605e73c09378a03d2e18f785f39db506c58bb21e
2.D Lalák Daniel <Daniel.Lalak@student.sps-cl.cz>
2024-04-02 16:44:54 +0200

    postup práce

---
+++ C#/uvod/uvod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvod
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            try {
+                Console.WriteLine("Toto je kalkulacka pro  dvou cisel");
+                Console.WriteLine(".");
+                Console.WriteLine(".");
+                Console.WriteLine("Zadejte prvni cislo.");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Zadejte druhe cislo.");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Zadejte znamenko");
+                string znamenko = Console.ReadLine();
+                if (znamenko == "+") {
+                    Console.WriteLine($"Vysledek je: {a + b}");
+                } else if (znamenko == "-") {
+                    Console.WriteLine($"Vysledek je: {a - b}");
+                } else if (znamenko == "*") {
+                    Console.WriteLine($"Vysledek je: {a * b}");
+                } else if (znamenko == "/" && b == 0) {
+                    Console.WriteLine("delit nulou nelze");
+                } else if (znamenko == "/") {
+                   Console.WriteLine($"Vysledek je: {a / b}");
+                };
+                Console.Read();
+            }
+            catch {
+                Console.WriteLine("Špatny vstup");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ práce_s_čísty/práce_s_čísty/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace práce_s_čísty
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            while (true) {
+                Console.WriteLine("Zadej sekvenci čísel oddělené čárkou.");
+                string vstup = Console.ReadLine();
+                string[] parts = vstup.Split(',');
+
+                try {
+                    int[] numbers = parts.Select(int.Parse).ToArray();
+                    int minNumber = numbers.Min();
+                    int maxNumber = numbers.Max();
+                    int longestAscendingInterval = GetLongestAscendingInterval(numbers);
+
+                    Console.WriteLine($"Nejmenší číslo je {minNumber}.");
+                    Console.WriteLine($"Největší číslo je {maxNumber}.");
+                    Console.WriteLine($"Délka nejdelšího vzestupného intervalu je {longestAscendingInterval}.");
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Znak není platné číslo, pokuste se problém upravit a zkuste to znovu.");
+                }
+            }
+        }
+        static int GetLongestAscendingInterval(int[] cisla) {
+            int maxLength = 0;
+            int currentLength = 1;
+
+            for (int i = 1; i < cisla.Length; i++) {
+                if (cisla[i] > cisla[i - 1]) {
+                    currentLength++;
+                }else {
+                    maxLength = Math.Max(maxLength, currentLength);
+                    currentLength = 1;
+                }
+            }
+
+            maxLength = Math.Max(maxLength, currentLength);
+            return maxLength;
+        }
+    }
+
+}
