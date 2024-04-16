e05a53548db7b654b2d8cbceadb4df1341f006a9
2.D Lalák Daniel <Daniel.Lalak@student.sps-cl.cz>
2024-04-16 16:52:20 +0200

    C# 3

---
+++ C#/aaa/aaa/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 3.hodina_2 samostatna prace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           class ComplexNumber
+            {
+            public double Real;
+            public double Imaginary;
+
+            public ComplexNumber(double real, double imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+
+            public ComplexNumber add(ComplexNumber other)
+            {
+                return new ComplexNumber(Real + other.Real, Imaginary + other.Imaginary);
+            }
+
+            public ComplexNumber substract(ComplexNumber other)
+            {
+                return new ComplexNumber(Real - other.Real, Imaginary - other.Imaginary);
+            }
+
+            public ComplexNumber multiply(ComplexNumber other)
+        {
+            return new ComplexNumber(Real * other.Real - Imaginary * other.Imaginary, Imaginary * other.Real + Real * other.Imaginary)
+        }
+            }
+        }
+    }
+}
